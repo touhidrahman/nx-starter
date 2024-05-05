c0ab8f4f1cb38a2325e824b3fc2049cc21dfc18b
Touhid Rahman <touhidrahman1987@gmail.com>
2024-05-04 20:54:58 -0400

    Add hono server

---
+++ apps/example-server/README.md
@@ -0,0 +1,1 @@
+# Server created with honojs
--- apps/example-server/src/main.ts
+++ apps/example-server/src/main.ts
@@ -1,1 +1,19 @@
-console.log('Hello World')
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+    return c.text('Hello Hono!')
+})
+app.get('/api', (c) => {
+    return c.json({ message: 'Hello Hono!' })
+})
+
+const port = 3000
+console.log(`Server is running on port ${port}`)
+
+serve({
+    fetch: app.fetch,
+    port,
+})
