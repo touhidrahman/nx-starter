f814509e0efd89f2133ce8fc4aef18436ac8124d
Mushfiqur Rahman Akhand <37366996+shohan-akhand@users.noreply.github.com>
2024-07-02 03:11:19 +0600

    user-management page created for ionic

---
+++ apps/example-ionic/src/app/pages/page-admin-userlist/page-admin-userlist.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { IonicModule } from '@ionic/angular'
+
+interface User {
+    name: string
+    mail: string
+    avatarUrl: string
+}
+
+@Component({
+    selector: 'myorg-page-admin-userlist',
+    standalone: true,
+    imports: [CommonModule, IonicModule],
+    templateUrl: './page-admin-userlist.component.html',
+    styleUrl: './page-admin-userlist.component.scss',
+})
+export class PageAdminUserlistComponent implements OnInit {
+    users: User[] = []
+
+    ngOnInit() {
+        this.users = [
+            {
+                name: 'Huey',
+                mail: 'huey123.abc',
+                avatarUrl:
+                    'https://ionicframework.com/docs/img/demos/avatar.svg',
+            },
+            {
+                name: 'Dewey',
+                mail: 'dewey123.abc',
+                avatarUrl:
+                    'https://ionicframework.com/docs/img/demos/avatar.svg',
+            },
+            {
+                name: 'Louie',
+                mail: 'louie123.abc',
+                avatarUrl:
+                    'https://ionicframework.com/docs/img/demos/avatar.svg',
+            },
+            {
+                name: 'Huey',
+                mail: 'huey123.abc',
+                avatarUrl:
+                    'https://ionicframework.com/docs/img/demos/avatar.svg',
+            },
+            {
+                name: 'Dewey',
+                mail: 'dewey123.abc',
+                avatarUrl:
+                    'https://ionicframework.com/docs/img/demos/avatar.svg',
+            },
+            {
+                name: 'Louie',
+                mail: 'louie123.abc',
+                avatarUrl:
+                    'https://ionicframework.com/docs/img/demos/avatar.svg',
+            },
+            {
+                name: 'Huey',
+                mail: 'huey123.abc',
+                avatarUrl:
+                    'https://ionicframework.com/docs/img/demos/avatar.svg',
+            },
+            {
+                name: 'Dewey',
+                mail: 'dewey123.abc',
+                avatarUrl:
+                    'https://ionicframework.com/docs/img/demos/avatar.svg',
+            },
+            {
+                name: 'Louie',
+                mail: 'louie123.abc',
+                avatarUrl:
+                    'https://ionicframework.com/docs/img/demos/avatar.svg',
+            },
+            {
+                name: 'Huey',
+                mail: 'huey123.abc',
+                avatarUrl:
+                    'https://ionicframework.com/docs/img/demos/avatar.svg',
+            },
+            {
+                name: 'Dewey',
+                mail: 'dewey123.abc',
+                avatarUrl:
+                    'https://ionicframework.com/docs/img/demos/avatar.svg',
+            },
+            {
+                name: 'Louie',
+                mail: 'louie123.abc',
+                avatarUrl:
+                    'https://ionicframework.com/docs/img/demos/avatar.svg',
+            },
+        ]
+    }
+}
