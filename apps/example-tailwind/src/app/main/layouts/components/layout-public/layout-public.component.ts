caf39d7c08d7437e95d213a3fb821150a181f69c
Mushfiqur Rahman Akhand <37366996+shohan-akhand@users.noreply.github.com>
2024-07-11 02:02:19 +0600

    new navbar and layout created for public

--- apps/example-tailwind/src/app/main/layouts/components/layout-public/layout-public.component.ts
+++ apps/example-tailwind/src/app/main/layouts/components/layout-public/layout-public.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { HeaderPublicComponent } from "../../../headers/components/header-public/header-public.component";
 
 @Component({
     selector: 'app-layout-public',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, HeaderPublicComponent],
     templateUrl: './layout-public.component.html',
     styleUrl: './layout-public.component.scss',
 })
