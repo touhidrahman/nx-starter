b3c5310489254e496e35ba887e53a4c226ec6827
Mushfiqur Rahman Akhand <37366996+shohan-akhand@users.noreply.github.com>
2024-07-10 08:25:56 +0600

    fix all UI component with full use of Spartan UI

--- apps/example-tailwind/src/app/pages/page-profile/page-profile.component.ts
+++ apps/example-tailwind/src/app/pages/page-profile/page-profile.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { SpartanModules } from '@myorg/spartan-modules'
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm'
 
 @Component({
     selector: 'app-page-profile',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule,...SpartanModules,HlmInputDirective],
     templateUrl: './page-profile.component.html',
     styleUrl: './page-profile.component.scss',
 })
--- apps/example-tailwind/src/app/pages/page-reset-password/page-reset-password.component.ts
+++ apps/example-tailwind/src/app/pages/page-reset-password/page-reset-password.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { SpartanModules } from '@myorg/spartan-modules'
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm'
 
 @Component({
     selector: 'app-page-reset-password',
     standalone: true,
-    imports: [CommonModule,...SpartanModules],
+    imports: [CommonModule,...SpartanModules,HlmInputDirective],
     templateUrl: './page-reset-password.component.html',
     styleUrl: './page-reset-password.component.scss',
 })
