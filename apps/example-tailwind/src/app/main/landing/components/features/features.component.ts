24cfd83f7eb739bbe04dc655cd517e043f67de43
ferdousr3 <ferdousr1992@gmail.com>
2024-08-14 00:55:22 +0600

    change features icon

--- apps/example-tailwind/src/app/main/landing/components/features/features.component.ts
+++ apps/example-tailwind/src/app/main/landing/components/features/features.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common'
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { SpartanModules } from '@myorg/spartan-modules'
 import { provideIcons } from '@ng-icons/core'
-import { lucideCheck } from '@ng-icons/lucide'
+import { lucideCheck, lucideShieldCheck } from '@ng-icons/lucide'
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm'
 import { LucideAngularModule } from 'lucide-angular'
 
@@ -20,7 +20,7 @@ import { features, Features } from './feature.data'
     templateUrl: './features.component.html',
     styleUrl: './features.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    providers: [provideIcons({ lucideCheck })],
+    providers: [provideIcons({ lucideCheck, lucideShieldCheck })],
 })
 export class FeaturesComponent {
     features: Features[] = features
