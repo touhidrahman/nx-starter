71a93171f9c43bd3898f99ddf50d2e0b1e66c6aa
Mushfiqur Rahman Akhand <37366996+shohan-akhand@users.noreply.github.com>
2024-09-17 13:50:27 +0600

    add sign up sign in funnnctionality for api call

--- apps/example-ionic/src/app/pages/page-login/page-login.component.ts
+++ apps/example-ionic/src/app/pages/page-login/page-login.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { IonicModule } from '@ionic/angular'
 import { RouterModule } from '@angular/router'
+import { LoginFormService } from '@myorg/common-auth'
+import { AuthStateService } from '@myorg/app-example-auth'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @Component({
     selector: 'myorg-page-login',
     standalone: true,
-    imports: [CommonModule,IonicModule,RouterModule],
+    imports: [CommonModule, IonicModule, RouterModule, ReactiveFormsModule],
     templateUrl: './page-login.component.html',
     styleUrl: './page-login.component.scss',
+    providers: [LoginFormService],
 })
-export class PageLoginComponent {}
+export class PageLoginComponent {
+    loginFormService = inject(LoginFormService)
+    private authStateService = inject(AuthStateService)
+
+    login() {
+        const formValues = this.loginFormService.getValue()
+
+        this.authStateService
+            .login(formValues.email, formValues.password)
+            .subscribe()
+    }
+}
