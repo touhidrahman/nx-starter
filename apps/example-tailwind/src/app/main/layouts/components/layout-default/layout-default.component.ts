d07f8ba81520718fd0941b780ddb91411e04be11
Mushfiqur Rahman Akhand <37366996+shohan-akhand@users.noreply.github.com>
2024-09-19 01:13:01 +0600

    complete homepage and cases page's UI part

--- apps/example-tailwind/src/app/main/layouts/components/layout-default/layout-default.component.ts
+++ apps/example-tailwind/src/app/main/layouts/components/layout-default/layout-default.component.ts
@@ -4,17 +4,19 @@ import { HeaderDefaultComponent } from '../../../headers/components/header-defau
 import { SidebarDefaultComponent } from '../../../sidebars/components/sidebar-default/sidebar-default.component'
 import { HeaderPublicComponent } from '../../../headers/components/header-public/header-public.component'
 import { CommonModule } from '@angular/common'
+import { PublicFooterComponent } from "../../../footer/public-footer/public-footer.component";
 
 @Component({
     selector: 'app-layout-default',
     standalone: true,
     imports: [
-        LucideAngularModule,
-        SidebarDefaultComponent,
-        HeaderDefaultComponent,
-        HeaderPublicComponent,
-        CommonModule,
-    ],
+    LucideAngularModule,
+    SidebarDefaultComponent,
+    HeaderDefaultComponent,
+    HeaderPublicComponent,
+    CommonModule,
+    PublicFooterComponent
+],
     templateUrl: './layout-default.component.html',
     styleUrl: './layout-default.component.scss',
 })
