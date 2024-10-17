6f9de3788c30fd97f0af9f0ce0ce4f9fb358e3d2
Touhid Rahman <touhidrahman1987@gmail.com>
2024-10-16 21:01:01 -0400

    Update packages

--- apps/example-server/src/core/configure-open-api.ts
+++ apps/example-server/src/core/configure-open-api.ts
@@ -1,7 +1,8 @@
 import { AppOpenAPI } from './core.type'
+import { apiReference } from '@scalar/hono-api-reference'
 
 export default function configureOpenAPI(app: AppOpenAPI) {
-    app.doc('/docs', {
+    app.doc('/doc', {
         openapi: '3.0.0',
         info: {
             title: 'Example Server',
@@ -9,4 +10,13 @@ export default function configureOpenAPI(app: AppOpenAPI) {
             description: 'Example server built with Hono',
         },
     })
+
+    app.get(
+        '/reference',
+        apiReference({
+            spec: {
+                url: '/doc',
+            },
+        }),
+    )
 }
