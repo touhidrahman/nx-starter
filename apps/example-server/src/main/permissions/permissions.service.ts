4370a3410fbf5c46129bc1122a08205a9036653b
Hmmurad <muradjpr@gmail.com>
2024-11-07 14:29:16 +0600

    fix delete all permission route

--- apps/example-server/src/main/permissions/permissions.service.ts
+++ apps/example-server/src/main/permissions/permissions.service.ts
@@ -1,6 +1,7 @@
 import { db } from '../../core/db/db'
 import { permissionsTable } from '../../core/db/schema'
 import { and, eq, getTableColumns } from 'drizzle-orm'
+import { SelectPermission } from './permissions.schema'
 
 export const listAll = async (limit: number = 100) => {
     return db
@@ -12,19 +13,16 @@ export const listAll = async (limit: number = 100) => {
 export const create = async (permissionData: any) => {
     return db.insert(permissionsTable).values(permissionData).returning()
 }
-//
-// export const deleteMany = async (permission: {
-//     groupId?: string
-//     role?: string
-//     area?: string
-// }) => {
-//     return db
-//         .delete(permissionsTable)
-//         .where(
-//             and(
-//                 eq(permissionsTable.groupId, permission.groupId),
-//                 eq(permissionsTable.role as any, permission.role),
-//                 eq(permissionsTable.area, permission.area),
-//             ),
-//         )
-// }
+
+export const deleteMany = async (permission: any) => {
+    return db
+        .delete(permissionsTable)
+        .where(
+            and(
+                eq(permissionsTable.groupId, permission.groupId),
+                eq(permissionsTable.role as any, permission.role),
+                eq(permissionsTable.area, permission.area),
+            ),
+        )
+}
+;``
