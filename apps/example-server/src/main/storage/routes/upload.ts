0019b77d63c3a92670b9417d43412a43bb066085
Touhid Rahman <touhidrahman1987@gmail.com>
2024-11-27 10:04:00 -0500

    Move to storage service

--- apps/example-server/src/main/storage/routes/upload.ts
+++ apps/example-server/src/main/storage/routes/upload.ts
@@ -1,21 +1,16 @@
 import { createRoute, z } from '@hono/zod-openapi'
-import { CREATED, NOT_FOUND, OK } from 'stoker/http-status-codes'
+import { CREATED } from 'stoker/http-status-codes'
+import { jsonContent } from 'stoker/openapi/helpers'
 import { AppRouteHandler } from '../../../core/core.type'
-import { zEmpty, zId } from '../../../core/models/common.schema'
+import { uploadToS3AndGetUrl } from '../../../core/third-party/s3.service'
 import { ApiResponse } from '../../../core/utils/api-response.util'
 import { checkToken } from '../../auth/auth.middleware'
 import {
     SelectStorage,
     zInsertStorage,
-    zSelectStorage,
     zUploadStorage,
 } from '../storage.schema'
-import { getStorageItemById } from '../storage.service'
-import { jsonContent } from 'stoker/openapi/helpers'
-import { uploadToS3AndGetUrl } from '../../../core/third-party/s3.service'
-import { db } from '../../../core/db/db'
-import { storageTable } from '../../../core/db/schema'
-import { getFileType } from '../storage.util'
+import { createStorageRecord } from '../storage.service'
 
 export const uploadRoute = createRoute({
     path: '/v1/storage/upload',
@@ -39,49 +34,36 @@ export const uploadHandler: AppRouteHandler<typeof uploadRoute> = async (c) => {
     const fileOrFiles = body['file']
     const entityId = body.entityId as string
     const entityName = body.entityName as string
+    const results: SelectStorage[] = []
 
     if (fileOrFiles instanceof Array) {
-        const results: SelectStorage[] = []
         for await (const f of fileOrFiles) {
             const file = f as File
             const url = await uploadToS3AndGetUrl(file)
-            const [item] = await db
-                .insert(storageTable)
-                .values({
-                    filename: file.name,
-                    url,
-                    type: getFileType(file),
-                    size: file.size,
-                    extension: file.name.split('.').pop(),
-                    entityId,
-                    entityName,
-                    uploadedBy: payload.sub,
-                })
-                .returning()
+            const [item] = await createStorageRecord({
+                file,
+                url,
+                entityName,
+                entityId,
+                uploadedBy: payload.sub,
+            })
             results.push(item)
         }
-        return c.json(
-            { data: results, message: 'File uploaded', success: true },
-            CREATED,
-        )
     } else {
         const file = fileOrFiles as File
         const url = await uploadToS3AndGetUrl(file)
-        const [item] = await db
-            .insert(storageTable)
-            .values({
-                filename: file.name,
-                url,
-                type: getFileType(file),
-                extension: file.name.split('.').pop(),
-                entityId,
-                entityName,
-                uploadedBy: payload.sub,
-            })
-            .returning()
-        return c.json(
-            { data: [item], message: 'File uploaded', success: true },
-            CREATED,
-        )
+        const [item] = await createStorageRecord({
+            file,
+            url,
+            entityName,
+            entityId,
+            uploadedBy: payload.sub,
+        })
+        results.push(item)
     }
+
+    return c.json(
+        { data: results, message: 'Files uploaded', success: true },
+        CREATED,
+    )
 }
--- apps/example-server/src/main/storage/storage.service.ts
+++ apps/example-server/src/main/storage/storage.service.ts
@@ -1,6 +1,7 @@
 import { eq, getTableColumns } from 'drizzle-orm'
 import { db } from '../../core/db/db'
 import { storageTable } from '../../core/db/schema'
+import { getFileType } from './storage.util'
 
 export const getStorageItemById = async (id: string) => {
     const [result] = await db
@@ -9,4 +10,26 @@ export const getStorageItemById = async (id: string) => {
         .where(eq(storageTable.id, id))
         .limit(1)
     return result ? result : null
+}
+
+export async function createStorageRecord(data: {
+    file: File,
+    url: string,
+    entityName: string,
+    entityId: string,
+    uploadedBy: string,
+}) {
+    return db
+        .insert(storageTable)
+        .values({
+            filename: data.file.name,
+            url: data.url,
+            type: getFileType(data.file),
+            size: data.file.size,
+            extension: data.file.name.split('.').pop(),
+            entityId: data.entityId,
+            entityName: data.entityName,
+            uploadedBy: data.uploadedBy,
+        })
+        .returning()
 }
