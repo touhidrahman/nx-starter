bb84b11f68f2a9b197c7181566dc6a8eb42225d7
Istiuak <darcygravan@gmail.com>
2024-12-01 17:32:17 +0600

    removed spartan from sidebars

--- apps/example-admin/src/app/main/layouts/components/layout-default/layout-default.component.ts
+++ apps/example-admin/src/app/main/layouts/components/layout-default/layout-default.component.ts
@@ -1,21 +1,12 @@
 import { Component } from '@angular/core'
-
-import { LucideAngularModule } from 'lucide-angular'
 import { HeaderDefaultComponent } from '../../../headers/components/header-default/header-default.component'
 import { SidebarDefaultComponent } from '../../../sidebars/components/sidebar-default/sidebar-default.component'
 import { CommonModule } from '@angular/common'
-import { SidebarAdminComponent } from '../../../sidebars/components/sidebar-admin/sidebar-admin.component'
 
 @Component({
     selector: 'app-layout-default',
     standalone: true,
-    imports: [
-        LucideAngularModule,
-        SidebarDefaultComponent,
-        HeaderDefaultComponent,
-        CommonModule,
-        SidebarAdminComponent,
-    ],
+    imports: [SidebarDefaultComponent, HeaderDefaultComponent, CommonModule],
     templateUrl: './layout-default.component.html',
     styleUrl: './layout-default.component.scss',
 })
