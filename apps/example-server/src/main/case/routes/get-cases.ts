1c22d1e4fdfb87d738d69be69de5f68c7b8fdb3a
hmmurad <muradjpr@gmail.com>
2024-12-19 01:46:07 +0600

    Fix previous commit and modify get cases route

--- apps/example-server/src/main/case/routes/get-cases.ts
+++ apps/example-server/src/main/case/routes/get-cases.ts
@@ -1,15 +1,10 @@
 import { createRoute, z } from '@hono/zod-openapi'
-import { OK, NOT_FOUND } from 'stoker/http-status-codes'
+import { OK } from 'stoker/http-status-codes'
 import { AppRouteHandler } from '../../../core/core.type'
-import { zEmpty } from '../../../core/models/common.schema'
 import { ApiResponse } from '../../../core/utils/api-response.util'
 import { checkToken } from '../../auth/auth.middleware'
 import { zSelectCase } from '../case.schema'
-import { zSelectGroup } from '../../group/group.schema'
-import { eq, getTableColumns, ilike, sql, SQL } from 'drizzle-orm'
-import { casesTable, groupsTable } from '../../../core/db/schema'
-import { toInt } from 'radash'
-import { db } from '../../../core/db/db'
+import { getAllCasesByGroupId } from '../case.service'
 
 export const getCasesRoute = createRoute({
     path: '/v1/cases',
@@ -17,15 +12,14 @@ export const getCasesRoute = createRoute({
     tags: ['Case'],
     middleware: [checkToken] as const,
     request: {
-        query: zSelectGroup.extend({
+        query: zSelectCase.extend({
             search: z.string().optional(),
-            size: z.string().optional(),
-            page: z.string().optional(),
+            page: z.number().int().optional().default(1),
+            size: z.number().int().optional().default(10),
         }),
     },
     responses: {
         [OK]: ApiResponse(z.array(zSelectCase), 'List of Cases'),
-        [NOT_FOUND]: ApiResponse(zEmpty, 'No cases found'),
     },
 })
 
@@ -33,61 +27,30 @@ export const getCasesHandler: AppRouteHandler<typeof getCasesRoute> = async (
     c,
 ) => {
     const payload = await c.get('jwtPayload')
-    const query = c.req.valid('query')
-    const groupId = payload?.groupId
+    const { search, page, limit } = c.req.query()
 
-    const conditions: SQL<unknown>[] = []
+    const pageNumber = Number(page)
+    const limitNumber = Number(limit)
 
-    if (!groupId) {
-        return c.json(
-            { message: 'Group ID is required', data: {}, success: false },
-            NOT_FOUND,
-        )
-    }
+    const { groupId } = payload || {}
+    const { data, meta } = await getAllCasesByGroupId({
+        groupId,
+        search,
+        page: pageNumber,
+        limit: limitNumber,
+    })
 
-    if (query.search) {
-        conditions.push(eq(casesTable.name, query.search))
-    }
-
-    if (query.search) {
-        const searchTerm = `%${query.search}%`
-        conditions.push(
-            sql`${ilike(casesTable.name, searchTerm)} OR ${ilike(
-                casesTable.court,
-                searchTerm,
-            )}`,
-        )
-    }
-
-    conditions.push(eq(casesTable.groupId, groupId))
-
-    const limit = query?.size ? toInt(query.size) : 10
-    const offset = query?.page ? (toInt(query.page) - 1) * limit : 0
-
-    const whereCondition =
-        conditions.length > 0
-            ? sql`${conditions.reduce(
-                  (acc, condition, index) =>
-                      index === 0 ? condition : sql`${acc} AND ${condition}`,
-                  sql``,
-              )}`
-            : undefined
-
-    const cases = await db
-        .select({
-            ...getTableColumns(groupsTable),
-        })
-        .from(casesTable)
-        .where(whereCondition)
-        .limit(limit)
-        .offset(offset)
-
-    if (cases.length === 0) {
-        return c.json(
-            { data: {}, message: 'No cases found', success: false },
-            NOT_FOUND,
-        )
-    }
-
-    return c.json({ data: cases, message: 'List of Cases', success: true }, OK)
+    return c.json(
+        {
+            data: data,
+            pagination: {
+                page: meta.page,
+                size: meta.limit,
+                total: meta.totalCount,
+            },
+            message: 'Cases list',
+            success: true,
+        },
+        OK,
+    )
 }
