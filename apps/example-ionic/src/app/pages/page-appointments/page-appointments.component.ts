30326be91699df8fd8719a4828e4f721a9bb476b
Mushfiqur Rahman Akhand <37366996+shohan-akhand@users.noreply.github.com>
2024-12-29 20:11:21 +0600

    add some pages and minor improvements

--- apps/example-ionic/src/app/pages/page-appointments/page-appointments.component.ts
+++ apps/example-ionic/src/app/pages/page-appointments/page-appointments.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { IonicModule } from '@ionic/angular'
 
 @Component({
     selector: 'myorg-page-appointments',
-    imports: [CommonModule],
+    imports: [CommonModule, IonicModule],
     templateUrl: './page-appointments.component.html',
     styleUrl: './page-appointments.component.scss',
 })
-export class PageAppointmentsComponent {}
+export class PageAppointmentsComponent {
+    isModalOpen = false
+
+    setOpen(isOpen: boolean) {
+        this.isModalOpen = isOpen
+    }
+}
