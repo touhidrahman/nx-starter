be17b78cb3ece698c34a328edba4e699df707bce
ferdousr3 <ferdousr1992@gmail.com>
2025-01-09 13:38:12 +0600

    added plan create modal

---
+++ apps/example-admin/src/app/main/plan/components/plan-create-dialog/plan-create-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-create-dialog',
+  imports: [],
+  templateUrl: './plan-create-dialog.component.html',
+  styleUrl: './plan-create-dialog.component.css'
+})
+export class PlanCreateDialogComponent {
+
+}
